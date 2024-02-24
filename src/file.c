266ce5f29a71da8909508a6fcfcaf058fbb40b4f
thradams <thiago.adams@gmail.com>
2024-02-24 20:00:00 -0300

    fix ut8 assert (from  branch mingodad@aa07dbd)

--- src/file.c
+++ src/file.c
@@ -1,11 +1,20 @@
+#include <errno.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
 
-void F(int i);
-_Owner int make();
-int main()
-{ int a1=1/0;
-    F(make());
-  
-#pragma cake diagnostic check "-Wtemp-owner"
+int main(void)
+{
+    setlocale(LC_ALL, "en_US.utf8");
 
- 
+    errno = 0;
+    if (fputwc(L'🍌', stdout) == WEOF)
+    {
+        if (errno == EILSEQ)
+            puts("Encoding error in fputwc.");
+        else
+            puts("I/O error in fputwc.");
+        return EXIT_FAILURE;
+    }
 }
