e7b96c78755c546f6a2e4c9ba323fc6ad33ea63c
thradams <thiago.adams@gmail.com>
2024-02-25 14:14:42 -0300

    cakeconfig sample

---
+++ src/cakeconfig.h
@@ -0,0 +1,10 @@
+
+#ifdef __linux__
+
+//#pragma dir "/usr/lib/gcc/x86_64-linux-gnu/11/include"
+//#pragma dir "/usr/local/include"
+//#pragma dir "/usr/include/x86_64-linux-gnu"
+//#pragma dir "/usr/include"
+
+#endif
+
