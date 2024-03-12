66b3cf027979418f2cec09c34100a2cedfd1895e
thradams <thiago.adams@gmail.com>
2024-03-12 16:46:50 -0300

    u

--- src/file.c
+++ src/file.c
@@ -1,10 +1,30 @@
-void destroy(_Owner int i);
-
+#include <stdio.h>
+extern char* score_cards[];
+//  count(buf,ch) counts ch in buf. if buf is
+//  NULL, report the total count since last NULL
+int count(char* buf, char ch)
+{
+    static int total = 0;
+    int n;
+    char* p;
+    if (buf)
+    {
+        n = total; total = 0; return n;
+    }
+    for (p = buf; *p; p++)
+        if (*p == ch) total++;
+    return 0;
+}
 int main()
 {
-    _Owner int i = 0;
-    int v = i;
-    destroy(v);
-    #pragma cake diagnostic check "-Wnon-owner-move"
+    int i;
+    char* s;
+    for (i = 0; ; i++)
+    {
+        if ((s = score_cards[i]))
+            (void) count(s, 'K');
+        else break;
+    }
+    printf("found %d strikeouts\n", count(0, ' '));
+    return 0;
 }
-#pragma cake diagnostic check "-Wmissing-destructor"
