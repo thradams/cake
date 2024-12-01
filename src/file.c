015f2a7ecc110fe3deb08c843d1fc9bf1cbe2eb3
thradams <thiago.adams@gmail.com>
2024-12-01 09:15:13 -0300

    edit mode

--- src/file.c
+++ src/file.c
@@ -1,12 +1,13 @@
-﻿int main()
-{
-  do
-  {
-     int i;
-     defer i = 1;     
-     if (i) break;
-     defer i = 3;     
-  }
-  while(0);
-}
+﻿/*
+  https://www.open-std.org/jtc1/sc22/wg14/www/docs/n3214.pdf
+*/
 
+int main(void)
+{
+    const int * const p;
+    static_assert(_Generic(p, const int * : 1 ));
+    static_assert(_Generic(typeof(p), const int * const: 1));
+  
+    static_assert(_Generic(int, int : 1));    
+    static_assert(_Generic(typeof("abc"), char [4]: 1));
+}
